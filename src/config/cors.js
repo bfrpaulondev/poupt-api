b81d248c3556b5fa8e7ae672ee4a216e029ec7c7
Bruno Paulon <bfrpaulondev@users.noreply.github.com>
2026-05-12 17:26:47 +0000

    Update CORS to allow Vercel deployment origin

--- src/config/cors.js
+++ src/config/cors.js
@@ -1,7 +1,19 @@
 const cors = require('cors');
 
+const allowedOrigins = [
+  'http://localhost:5173',
+  'http://localhost:3000',
+  process.env.CLIENT_URL,
+].filter(Boolean);
+
 const corsOptions = {
-  origin: process.env.CLIENT_URL || 'http://localhost:5173',
+  origin: function (origin, callback) {
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, true);
+    }
+  },
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
